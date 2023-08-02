e2220310473332851074e3501b3b12a8eefa1c04
Sree-Ram-MS <srmsm3551@gmail.com>
2023-08-02 15:44:11 +0530

    commit

--- lib/dummyData.dart
+++ lib/dummyData.dart
@@ -3,30 +3,30 @@ const List<Map<String,dynamic>> dummyproducts=[
   "id" :1,
 "name":"product 1",
 "description":"very nice product with name product 1",
-"image":"assets/images/apple.jpg"
+"image":"assets/images/4k-superman-minimal-superheroes-darkness.jpg"
 },
 {
 "id" :2,
 "name":"product 2",
 "description":"very nice product with name product 2",
-"image":"assets/images/fruit1.jfif"
+"image":"assets/images/4k-superman-minimal-superheroes-darkness.jpg"
 },
 {
 "id" :3,
 "name":"product 3",
 "description":"very nice product with name product 3",
-"image":"assets/images/images.png"
+"image":"assets/images/super.jpg"
 },
 {
 "id" :4,
 "name":"product 4",
 "description":"very nice product with name product 4",
-"image":"assets/images/ceena.jfif"
+"image":"assets/images/super.jpg"
 },
 {
 "id" :5,
 "name":"product 5",
 "description":"very nice product with name product 5",
-"image":"assets/images/anime.jfif"
+"image":"assets/images/200805.jpg"
 },
 ];
