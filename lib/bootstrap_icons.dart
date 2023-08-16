91d35fe15044ef8988505ceaf00727ea969af049
Sree-Ram-MS <srmsm3551@gmail.com>
2023-08-16 17:00:14 +0530

    commit

---
+++ lib/bootstrap_icons.dart
@@ -0,0 +1,30 @@
+import 'package:bootstrap_icons/bootstrap_icons.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyApp',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const <Widget>[
+              Icon(BootstrapIcons.alarm),
+              Text('Alarm'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
